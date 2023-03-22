783a7cff03f34fe075b86e92f9de109cbba28124
snowcying <347025712@qq.com>
2023-03-22 23:07:45 +0800

    增加删除功能

--- src/views/BlogDetail.vue
+++ src/views/BlogDetail.vue
@@ -2,12 +2,27 @@
   <div>
     <Header></Header>
 
+    <el-dialog
+        title="警告"
+        :visible.sync="dialogVisible"
+        width="30%"
+        >
+      <span>确定删除文章吗？</span>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="handleClose">确 定</el-button>
+  </span>
+    </el-dialog>
+
     <div class="mblog" id="mblog">
       <h2> {{ blog.title }}</h2>
       <el-link  icon="el-icon-edit" v-if="ownBlog">
         <router-link :to="{name: 'BlogEdit', params: {blogId: blog.id}}" >
           编辑
         </router-link>
+      </el-link>
+      <el-link  icon="el-icon-delete" v-if="ownBlog">
+        <el-link type="danger" @click="dialogVisible = true">删除</el-link>
       </el-link>
       <el-divider></el-divider>
       <div class="markdown-body" v-html="blog.content"></div>
@@ -20,6 +35,7 @@
 <script>
 import 'github-markdown-css'
 import Header from "../components/Header";
+import Element from "element-ui";
 
 export default {
   name: "BlogDetail.vue",
@@ -31,7 +47,8 @@ export default {
         title: "",
         content: ""
       },
-      ownBlog: false
+      ownBlog: false,
+      dialogVisible: false,
     }
   },
   created() {
@@ -51,7 +68,24 @@ export default {
       _this.ownBlog = (blog.userId === _this.$store.getters.getUser.id)
 
     })
+  },
+methods:{
+  handleClose() {
+    const blogId = this.$route.params.blogId
+    const _this = this
+    this.$axios.delete('blog/'+blogId,{
+      headers: {
+        "Authorization": localStorage.getItem("token")
+      }
+    })
+        .then(res => {
+          _this.$router.push("/")
+          Element.Message.success("删除成功", {duration: 2 * 1000})
+          // done();
+        })
+        .catch(_ => {});
   }
+},
 }
 </script>
 
